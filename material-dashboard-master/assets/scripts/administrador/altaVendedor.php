2892240c5f29f8ec5f073780c2f6d5001ed0bc67
Nicolas Moreno <morenodurann@gmail.com>
2021-03-30 12:28:49 -0600

    asd

---
+++ material-dashboard-master/assets/scripts/administrador/altaVendedor.php
@@ -0,0 +1,9 @@
+<?php
+
+require('../../DataBases/conexion.php');
+
+$id = $_POST['id'];
+
+$query = "UPDATE Usuario SET tipoUsuario='Vendedor' WHERE id = ${id}";
+$query = $cn->prepare($query);
+$query->execute();
