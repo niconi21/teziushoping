fe060aa2e24ddbe9e83c997017136a01dc751ae6
Nicolas Moreno <morenodurann@gmail.com>
2021-03-30 13:23:26 -0600

    asd

---
+++ material-dashboard-master/assets/scripts/administrador/informeVenta.php
@@ -0,0 +1,23 @@
+<?php
+
+require('../../DataBases/conexion.php');
+
+
+
+$query = "SELECT v.fechaVenta, 
+a.nombreArticulo, a.imagen as imagenArticulo, a.precio as precioArticulo, 
+u.nombre as nombreComprador, u.apellidos as apellidosComprador, 
+m.tipoCuenta, 
+ven.nombre as nombreVendedor, ven.apellidos as apellidosVendedor, 
+c.nombre as categoriaNombre 
+FROM Ventas as v 
+INNER JOIN Articulo as a ON v.idArticulo = a.idArticulo
+INNER JOIN Usuario as u ON v.idUsuario = u.idUsuario
+INNER JOIN MetodoPago as m ON v.idMetodoPago = m.idMetodoPago
+INNER JOIN Usuario as ven ON a.idUsuario = ven.idUsuario
+INNER JOIN CategoriaArticulo as c ON a.idCategoria = c.idCategoria
+";
+
+$query = $cn->prepare($query);
+$query->execute();
+$results = $query->fetchAll(PDO::FETCH_OBJ);
