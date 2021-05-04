cac4ec07e710d9229112114287996b1f769a0c48
manolo2603 <manuelfrancis44@gmail.com>
2021-05-03 21:05:03 -0500

    eliminarUsuario

--- material-dashboard-master/assets/scripts/usuarios/insertarUsuarios.php
+++ material-dashboard-master/assets/scripts/usuarios/insertarUsuarios.php
@@ -1,12 +1,7 @@
 <?php
 
 
-$nombre = $_POST['nombreUsuario'];
-$apellidos = $_POST['apellidosUsuario'];
-$usuario = $_POST['nombreUsuario'];
-$contrasenia= $_POST['contrasenia'];
-$correo=$_POST['correoUsuario'];
-$telefono = $_POST['telefonoUsuario'];
+$idUsuario = $_POST['idUsuario'];
 $idDireccion=$_POST['1'];
 $fecha="CURDATE()";
 
@@ -15,8 +10,8 @@ try{
 
 
 
-$sql = $cn-> prepare("insert into Usuario(nombre, apellidos, nombreUsuario, contrasenia, idDireccion, fechaIngreso, tipoUsuario, telefono, correo) value ('?','?','?','?',?,'?','?','?','?')");
-$resultado=$sql->execute([$nombre,$apellidos,$usuario,$contrasenia,$idDireccion,$fecha,"Comprador",$telefono,$correo]);
+$sql = $cn-> prepare("delete from usuario where idUsuario=".$idUsuario);
+
 
 header('location: ../../index.html');
 }catch(Exception $e){
