4fc16fccef93a8909a96418dc4e27616ac104d46
Nicolas Moreno <morenodurann@gmail.com>
2021-05-08 02:39:12 -0500

    resmaterizacion de vista

--- material-dashboard-master/assets/views/pages/altaProducto.misPublicaciones.php
+++ material-dashboard-master/assets/views/pages/altaProducto.misPublicaciones.php
@@ -13,7 +13,7 @@ include('../partials/navbar.php');
 <div class="container-fluid">
 
     <div class="row">
-        <div class="col-md-8 offset-md-2 ">
+        <div class="col-md-4 offset-md-2">
             <div class="card bg-card ">
                 <div class="card-header text-center">
                     <h4 class="mt-3"><b>Publicar un producto/servicio</b></h4>
@@ -32,20 +32,48 @@ include('../partials/navbar.php');
                                     <div class="input-group-prepend">
                                         <span class="input-group-text" id="basic-addon1">@</span>
                                     </div>
-                                    <textarea name="" id="" cols="30" rows="10"></textarea>
+                                    <select name="" id="" class="form-control" required>
+                                        <option value="">Seleccióna uno</option>
+                                    </select>
                                 </div>
                                 <div class="input-group mb-3">
                                     <div class="input-group-prepend">
                                         <span class="input-group-text" id="basic-addon1">@</span>
                                     </div>
-                                    <input type="text" class="form-control" placeholder="Nombre" required>
+                                    <input type="number" class="form-control" placeholder="Precio MXN" required>
                                 </div>
+
+                                <div class="input-group mb-3">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text" id="basic-addon1">@</span>
+                                    </div>
+                                    <input type="text" class="form-control" placeholder="Cantidad" required>
+                                </div>
+                                <div class="input-group mb-3">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text" id="basic-addon1">@</span>
+                                    </div>
+                                    <textarea class="form-control" rows="5" required></textarea>
+                                </div>
+                                <div class="input-group mb-3">
+
+                                    <input type="file" class="form-control">
+                                </div>
+
+                                <input type="submit" class="btn btn-dark btn-block" value="Agregar producto/servicio">
                             </div>
-                            <div class="col">
-                            </div>
+
                         </div>
                     </form>
                 </div>
+            </div>
+        </div>
+        <div class="col-md-4 offset-md-1 mt-5">
+            <div class="card bg-card" style="width: 300px;">
+                <div class="card-header">
+                <h5><b><i>Imagen del producto</i></b></h5>
+                </div>
+                <img src="../../img/SinImagen.jpg" alt="" width="300px">
             </div>
         </div>
     </div>
