2f0bfd2f9759481d2c36fe355a565bc7be919e87
Nicolas Moreno <morenodurann@gmail.com>
2021-05-22 22:20:36 -0500

    cambios

--- app/DataBases/conexion.php
+++ app/DataBases/conexion.php
@@ -3,8 +3,8 @@
 
     //Definimos los parametros de usuario y contraseña para conectar a la BD
     $user = "root";
-    // $pwd = "teziushoping";
-    $pwd = "root";
+    $pwd = "teziushoping";
+    // $pwd = "root";
     $dbname = "TeziushopingDB";
 
     //Valida que los parametros coincidan y deja conectar
@@ -12,7 +12,7 @@
         $cn = new PDO('mysql:host=localhost; dbname='.$dbname, $user, $pwd,
         array(PDO::MYSQL_ATTR_INIT_COMMAND => "Set names utf8"));
 
-          
+
     //Si no mandará una excepción y no dejará conectar a la BD
     }catch (Exception $ex){
         echo "Ha ocurrido el error ".$ex->getMessage();
