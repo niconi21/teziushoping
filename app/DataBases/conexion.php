7417056b6a740943999c7f35c408f048cfdee8fb
Nicolas Moreno <morenodurann@gmail.com>
2021-05-24 14:13:57 -0500

    asd

--- app/DataBases/conexion.php
+++ app/DataBases/conexion.php
@@ -3,8 +3,8 @@
 
     //Definimos los parametros de usuario y contraseña para conectar a la BD
     $user = "root";
-    $pwd = "root";
     $pwd = "teziushoping";
+    $pwd = "root";
     $dbname = "TeziushopingDB";
 
     //Valida que los parametros coincidan y deja conectar
