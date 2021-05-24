564c4a637142dd685c876ddc5a56ba7d4dd561f5
Nicolas Moreno <morenodurann@gmail.com>
2021-05-24 13:44:16 -0500

    cambios

--- app/scripts/metodosPago/obtenerUnMetodoPago.php
+++ app/scripts/metodosPago/obtenerUnMetodoPago.php
@@ -18,7 +18,6 @@ $query = $cn->prepare('SELECT * FROM MetodoPago WHERE id=?');
 $query->execute([$id]);
 $result = $query->fetchAll(PDO::FETCH_OBJ)[0];
 if (isset($_SESSION['idMetodoPagoUpdate'])) {
-    unset($_SESSION['idMetodoPagoUpdate']);
     if ($query->rowCount() < 1) {
         header('location: ../../views/pages/misMetodosPago.php?statusPut=404');
     }
