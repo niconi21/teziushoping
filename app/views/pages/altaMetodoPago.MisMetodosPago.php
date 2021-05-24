97412ead2d0401bb731e239bd52ec635fd337bad
Nicolas Moreno <morenodurann@gmail.com>
2021-05-24 18:26:23 -0500

    formulario

--- app/views/pages/altaMetodoPago.MisMetodosPago.php
+++ app/views/pages/altaMetodoPago.MisMetodosPago.php
@@ -13,7 +13,7 @@ include('../partials/navbar.php');
 $tipo = $_GET['tipo'];
 $_SESSION['idMetodoPagoUpdate'] = $_GET['id'];
 if ($tipo == 'put') {
-    
+
     $urls['altaMetodoPago'] = '<a href="../pages/misMetodosPago.php">Mis métodos de pago</a> > Actualizar de método de pago';
 
     include('../../scripts/metodosPago/obtenerUnMetodoPago.php');
@@ -30,84 +30,149 @@ if ($tipo == 'put') {
 </div>
 
 <div class="container mt-5">
-    <div class="row animate__animated animate__fadeInLeft">
-        <div class="col-6 offset-3">
-            <form action="
-            <?php echo $tipo == 'put'? '../../scripts/metodosPago/actualizarMetodoPago.php' : '../../scripts/metodosPago/altaMetodoPago.php'?>" method="POST">
-                <div class="card  bg-fondo text-white">
-                    <div class="card-header text-center">
-                        <?php echo $tipo == 'put' ? 'Actualizar' : 'Agregar'; ?> método de pago
+    <form class="row animate__animated animate__fadeInLeft" action="
+            <?php echo $tipo == 'put' ? '../../scripts/metodosPago/actualizarMetodoPago.php' : '../../scripts/metodosPago/altaMetodoPago.php' ?>" method="POST">
+        <div class="col-6">
+            <div class="card  bg-fondo text-white">
+                <div class="card-header text-center">
+                    <?php echo $tipo == 'put' ? 'Actualizar' : 'Agregar'; ?> método de pago
+                </div>
+                <div class="card-body">
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="basic-addon1"><i class="fa fa-info-circle" aria-hidden="true"></i>
+                            </span>
+                        </div>
+                        <input type="text" class="form-control mb-4" placeholder="Nombre del titular" name="titular" value="<?php echo $result->titular ?>" required>
                     </div>
-                    <div class="card-body">
-                        <div class="input-group mb-3">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text" id="basic-addon1"><i class="fa fa-info-circle" aria-hidden="true"></i>
-                                </span>
-                            </div>
-                            <input type="text" class="form-control" placeholder="Nombre del titular" name="titular" value="<?php echo $result->titular?>" required>
-                        </div>
-                        <div class="input-group mb-3">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text" id="basic-addon1"><i class="fa fa-credit-card-alt" aria-hidden="true"></i>
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="basic-addon1"><i class="fa fa-credit-card-alt" aria-hidden="true"></i>
 
-                                </span>
-                            </div>
-                            <input type="text"  maxlength="16" minlength="16" class="form-control" placeholder="Número de tarjeta" name="tarjeta" value="<?php echo $result->noTarjeta?>" required>
+                            </span>
                         </div>
-                        <label>Fecha de expiración</label>
-                        <div class="row">
-                            
-                            <div class="col">
-                                <div class="input-group mb-3">
-                                    <div class="input-group-prepend">
-                                        <span class="input-group-text" id="basic-addon1">Mes
-                                        </span>
-                                    </div>
-                                    <select name="mes">
+                        <input type="text" maxlength="16" minlength="16" class="form-control" placeholder="Número de tarjeta" name="tarjeta" value="<?php echo $result->noTarjeta ?>" required>
+                    </div>
+                    <label>Fecha de expiración</label>
+                    <div class="row">
+
+                        <div class="col">
+                            <div class="input-group mb-3">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text" id="basic-addon1">Mes
+                                    </span>
+                                </div>
+                                <select name="mes">
                                     <?php
-                                    for ($i=1; $i <= 12; $i++) { 
-                                        echo '<option value="'.$i.'" '.($result->mes == $i ? 'selected' : '').'>'.$i.'</option>';
+                                    for ($i = 1; $i <= 12; $i++) {
+                                        echo '<option value="' . $i . '" ' . ($result->mes == $i ? 'selected' : '') . '>' . $i . '</option>';
                                     }
                                     ?>
-                                        
-                                        
-                                    </select>
-                                </div>
-                            </div>
-                            <div class="col">
-                                <div class="input-group mb-3">
-                                    <div class="input-group-prepend">
-                                        <span class="input-group-text" id="basic-addon1">Año
-                                        </span>
-                                    </div>
-                                    <select name="anio">
-                                        <?php
-                                        for ($i = 2021; $i < 2030; $i++) {
-                                            echo '<option value="'.$i.'" '.($result->anio == $i ? 'selected' : '').'>' . $i . '</option>';
-                                        }
-                                        ?>
-                                    </select>
-                                </div>
-                            </div>
 
+
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="input-group mb-5">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text" id="basic-addon1">Año
+                                    </span>
+                                </div>
+                                <select name="anio">
+                                    <?php
+                                    for ($i = 2021; $i < 2030; $i++) {
+                                        echo '<option value="' . $i . '" ' . ($result->anio == $i ? 'selected' : '') . '>' . $i . '</option>';
+                                    }
+                                    ?>
+                                </select>
+                            </div>
                         </div>
 
-                        <div class="input-group mb-3">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text" id="basic-addon1"><i class="fa fa-credit-card-alt" aria-hidden="true"></i>
+                    </div>
 
-                                </span>
-                            </div>
-                            <input type="password" class="form-control" placeholder="CCV" name="ccv" value="<?php echo $result->ccv;?>" required>
-                            <i class="fa fa-question-circle fa-2x ml-2 " aria-hidden="true" data-toggle="tooltip" data-placement="top" title="Tooltip on top"></i>
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="basic-addon1"><i class="fa fa-credit-card-alt" aria-hidden="true"></i>
 
-
+                            </span>
                         </div>
-                        <div class="card-footer text-muted text-center">
-                            <button type="submit" class="btn btn-success"><?php echo $tipo == 'put' ? 'Actualizar' : 'Agregar'; ?> método de pago</button>
+                        <input type="password" class="form-control" placeholder="CCV" name="ccv" value="<?php echo $result->ccv; ?>" required>
+                        <i class="fa fa-question-circle fa-2x ml-2 " aria-hidden="true" data-toggle="tooltip" data-placement="top" title="Tooltip on top"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-6">
+            <div class="card  bg-fondo text-white">
+                <div class="card-header">Domicilio</div>
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="input-group mb-3">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text" id="basic-addon1"><i class="fa fa-info-circle" aria-hidden="true"></i>
+                                    </span>
+                                </div>
+                                <input type="text" class="form-control" placeholder="Calle" name="calle" value="<?php echo $_SESSION['calle']; ?>" required>
+                            </div>
+                            <div class="input-group mb-3">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text" id="basic-addon1"><i class="fa fa-info-circle" aria-hidden="true"></i>
+                                    </span>
+                                </div>
+                                <input type="text" class="form-control" placeholder="Ciudad" name="ciudad" value="<?php echo $_SESSION['ciudad']; ?>" required>
+                            </div>
+                            <div class="input-group mb-3">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text" id="basic-addon1"><i class="fa fa-info-circle" aria-hidden="true"></i>
+                                    </span>
+                                </div>
+                                <input type="text" class="form-control" placeholder="No. Ext" name="noExt" value="<?php echo $_SESSION['noExt']; ?>" required>
+
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="input-group mb-3">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text" id="basic-addon1"><i class="fa fa-info-circle" aria-hidden="true"></i>
+                                    </span>
+                                </div>
+                                <input type="text" class="form-control" placeholder="Colonia" name="colonia" value="<?php echo $_SESSION['colonia']; ?>" required>
+                            </div>
+                            <div class="input-group mb-3">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text" id="basic-addon1"><i class="fa fa-info-circle" aria-hidden="true"></i>
+                                    </span>
+                                </div>
+                                <input type="text" class="form-control" placeholder="C.P" name="cp" value="<?php echo $_SESSION['cp']; ?>" required>
+                            </div>
+                            <div class="input-group mb-3">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text" id="basic-addon1"><i class="fa fa-info-circle" aria-hidden="true"></i>
+                                    </span>
+                                </div>
+                                <input type="text" class="form-control" placeholder="No. Int" name="noInt" value="<?php echo $_SESSION['noInt']; ?>">
+                            </div>
+
                         </div>
                     </div>
-            </form>
+
+
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="basic-addon1"><i class="fa fa-info-circle" aria-hidden="true"></i>
+                            </span>
+                        </div>
+                        <textarea rows="5" class="form-control" name="referencias" placeholder="Referencias..."><?php echo $_SESSION['referencias']; ?></textarea>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </form>
+    <div class="row">
+        <div class="col">
+            <button type="submit" class="btn btn-success btn-block"><?php echo $tipo == 'put' ? 'Actualizar' : 'Agregar'; ?> método de pago</button>
         </div>
     </div>
 </div>
